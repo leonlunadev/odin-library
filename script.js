c75a8cb26f5647f8333903e6f7bd7485027f1861
Roberto Leon-Luna <leonlunadev@gmail.com>
2024-11-12 16:49:01 -0500

    creates text node when new book button is clicked

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function Book() {}
 function addBookToLibrary() {}
 
 function displayBooks() {}
+
+const NewBook = document.querySelector("button");
+
+NewBook.addEventListener("click", NewBookTest);
+
+function NewBookTest() {
+  const NewDiv = document.createElement("div");
+  NewDiv.appendChild(document.createTextNode("New Book!"));
+  document.body.appendChild(NewDiv);
+}
