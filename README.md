b5359f11ff389c52e5385d7a756ebe2017541f3b
Roberto Leon-Luna <leonlunadev@gmail.com>
2024-11-23 19:01:53 -0500

    adds some content to the form

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Form for creating a book
 bookshelf
 
 book
+
+add actual form to form
+handle submisison of form
+add book to book array
+add book from book array to screen
+
+book remove button, book read button
--- script.js
+++ script.js
@@ -12,9 +12,43 @@ NewBook.addEventListener("click", NewBookTest);
 
 function NewBookTest(e) {
   const popup = document.createElement("div");
-  const form = document.createElement("div");
+  const formWindow = document.createElement("div");
+  const form = document.createElement("form");
   popup.className += "popup";
-  form.className += "form";
-  popup.appendChild(form);
+  formWindow.className += "formWindow";
+
+  const header = document.createElement("h1");
+  header.textContent = "Book Information";
+
+  const titleLabel = document.createElement("label");
+  titleLabel.setAttribute("for", "title");
+  titleLabel.textContent = "Title: ";
+
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("placeholder", "Enter book title");
+  titleInput.setAttribute("name", "title");
+
+  const authorLabel = document.createElement("label");
+  authorLabel.setAttribute("for", "author");
+  authorLabel.textContent = "Author: ";
+
+  const authorInput = document.createElement("input");
+  authorInput.setAttribute("type", "text");
+  authorInput.setAttribute("placeholder", "Enter author's name");
+  authorInput.setAttribute("name", "author");
+
+  const submitButton = document.createElement("button");
+  submitButton.setAttribute("type", "submit");
+  submitButton.textContent = "Add Book";
+
+  form.appendChild(titleLabel);
+  form.appendChild(titleInput);
+  form.appendChild(authorLabel);
+  form.appendChild(authorInput);
+  form.appendChild(submitButton);
+  formWindow.appendChild(header);
+  formWindow.appendChild(form);
+  popup.appendChild(formWindow);
   document.body.appendChild(popup);
 }
